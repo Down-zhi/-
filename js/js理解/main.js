52da4bdff54aa8fc071de01e4d53a5580b76a0e6
Guodaxia <1265986077@qq.com>
2024-05-01 22:01:20 +0800

    feat:js学习

--- js/js理解/main.js
+++
@@ -1,5 +0,0 @@
-import {add,subtract,multiply,random} from 'math.js'
-console.log(add(5, 3)); 
-console.log(subtract(5, 3)); 
-console.log(multiply(5, 3))
-console.log(random(1,100));
--- js/js理解/math.js
+++ js/js理解/math.js
@@ -11,3 +11,20 @@ export function add(a, b) {
  export  function random(max, min) {
     return Math.floor(Math.random() * (max - min) + min);
 };
+export const name='ikun'
+
+// 等价于这样写export{ add,subtract,multiply,name}
+
+// const obj={
+//   foo:'foo'
+// }
+// export default obj;   //一个模块只有一个默认抛出
+class Person{
+  constructor(){
+  }
+    sayAge(){
+      console.log('16');
+    }
+ 
+}
+export default Person;
